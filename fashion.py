46f7245793147d010f4f7c17ecca04f9cca26071
Carolina Amorim <carolinaoa1@al.insper.edu.com>
2026-04-07 16:53:16 -0300

    pequenos ajustes + ideia inicial das perguntas

---
+++ fashion.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import re
+
+# ler e filtrar o data set
+
+df = pd.read_csv("fashion_data_2018_2022.csv")
+
+COLUNAS = [
+    "product_id",
+    "product_name",
+    "category",
+    "pattern",
+    "season",
+    "sales_count",
+    "average_rating",
+    "wish_list_count",
+    "year_of_sale"
+]
+
+# filtra o dataset para as 10 colunas -> escolhi oque eu acho que vai ser mais interessante para as perguntas
+df_filtered = df[COLUNAS].copy()
+
+# "normalizar" e padronizar as tabelas para o prolog conseguir ler
+def normalizar_data(data):
+    text = str(data).strip().lower()
+    text = text.replace(" ", "_").replace("-", "_").replace("/", "_")
+    text = re.sub(r"[^a-z0-9_]", "", text)
+    return text
+
+with open("base_fashion.pl", "w", encoding="utf-8") as f: # se nao existir o arquivo ele cria
+
+    # percorre o arquivo filtrado e normaliza/padroniza os dados
+    for _, row in df_filtered.iterrows():
+        product_id = int(row["product_id"])
+        product_name = normalizar_data(row["product_name"])
+        category = normalizar_data(row["category"])
+        pattern = normalizar_data(row["pattern"])
+        season = normalizar_data(row["season"])
+        sales_count = int(row["sales_count"])
+        average_rating = float(row["average_rating"])
+        wish_list_count = int(row["wish_list_count"])
+        year_of_sale = int(row["year_of_sale"])
+
+        line = (
+            f"peca({product_id}, {product_name}, {category}, {pattern}, {season}, {sales_count}, {average_rating}, {out_of_stock_times}, {wish_list_count}, {year_of_sale}).\n"
+        )
+        f.write(line)
---
+++ queries.pl
@@ -0,0 +1,5 @@
+# arquivo com as regras e as perguntas
+
+# 1) Qual estampa é a favorita para a primavera? -> com o maior wish_list_count
+# 2) Em qual ano os biker shorts foram tendência? -> com o maior sales_count
+# 3) Qual foi o melhor produto do inverno -> com o avarage rating
